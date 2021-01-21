4968f184adc56bc5f53f3d08fc9bafcc2fdec1af
Nursultan-Turatbekov <nursbaisalov@gmail.com>
2021-01-21 20:11:03 +0600

    added styles

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , HttpResponse, redirect
 from .models import ToDo
 from .models import Book
 
@@ -28,3 +28,18 @@ def second(request):
 
 def work(request):
     return render(request, "work.html")
+
+
+def add_todo(request):
+    form = request.POST
+    text = form["todo_text"]
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test)  
+
+def add_book(request):
+    form = request.POST
+    text = form["book_text"] 
+    book = Book(text=test)
+    book.save()
+    return redirect(books)   
